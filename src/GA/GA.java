bab5b9a26fc73bdb5db7a18098ddba50eafc9d8d
Janssen Rodrigues <faker.rdc@hotmail.com>
2018-03-26 15:39:21 -0300

    Change simple array to Object class

--- src/GA/GA.java
+++ src/GA/GA.java
@@ -13,22 +13,25 @@ import java.util.Vector;
  *
  * @author Janssen
  */
-public class GA {
-    private static final int POP = 20;
-    static int[] individuo = new int[6];
-    static int[][] populacao = new int[30][6];
-    static int[][] populacaoOrdanada = new int[30][6];
+public class GA extends Individuo{
+    private static final int POP = 3;    
+    static Individuo[] populacao = new Individuo[6];
+    static Individuo[] populacaoOrdenada = new Individuo[6];
     
     static Random rnd = new Random();
     
     public static void gerarPopulacao(){
         System.out.println("POPULACAO INICIAL COM GENES: ");
+        
         for(int i = 0; i < POP; i++){
-            for(int k = 0; k < individuo.length; k++){
+            Individuo individuo = new Individuo();
+            populacao[i] = individuo;
+//            int[] individuo = new int[6];
+            for(int k = 0; k < individuo.getGenes().length; k++){
                 if(rnd.nextDouble() < 0.5){
-                    populacao[i][k] = 0;
+                    populacao[i].genes[k] = 0;
                 } else {
-                    populacao[i][k] = 1;
+                    populacao[i].genes[k] = 1;
                 }       
                 populacao[i] = individuo;
             }
@@ -36,27 +39,36 @@ public class GA {
 //            calcularAptidao(populacao[i]); //INDIVIDUO i DENTRO DE POPULACAO.
 //            System.out.println(Arrays.toString(populacao[i]));
             System.out.print("INDIVIDUO " + i + " - ");
-            System.out.println(Arrays.toString(populacao[i]));
+            System.out.println(Arrays.toString(populacao[i].getGenes()));
             calcularAptidao(populacao[i]);
         }
     }
     
-    public static void calcularAptidao(int[] individuo){
+    public static void calcularAptidao(Individuo individuo){
 //        System.out.println(Arrays.toString(individuo));
         int aptidao = 0;
-        for(int i = 0; i < individuo.length; i++){
-            aptidao += individuo[i];
+        for(int i = 0; i < individuo.getGenes().length; i++){
+            aptidao += individuo.genes[i];
         }
+        individuo.setAptidao(aptidao);
 //        System.out.println(Arrays.toString(individuo));
+        
         System.out.println("Aptidao : " + aptidao);
         System.out.println("----------------");
     }
     
-    public static void ordenarPopulacao(int[][] populacao){
+    public static void ordenarPopulacao(Individuo[] populacao){
+        
         System.out.println("ORDENANDO POPULACAO");
-        for(int i = 0; i <  populacao.length; i++) {
-            System.out.println("individuo " + i);
-            System.out.println(Arrays.toString(populacao[i]));
+        for(int i = 6; i > 0; i--) {
+//            System.out.println(i);
+//            System.out.println(populacao[i].aptidao);
+            for(int k = 0; k < populacao.length; k++) {
+//                System.out.println(populacao[k].getAptidao());
+                if(populacao[k].getAptidao() == i){
+                    populacaoOrdenada[k] = populacao[k];
+                }
+            }
         }
     }
     
@@ -64,10 +76,11 @@ public class GA {
         // TODO code application logic here
         gerarPopulacao();
 //        ordenarPopulacao(populacao);
-        for(int i = 0; i <  populacao.length; i++) {
-            System.out.print("individuo " + i + " - ");
-            System.out.println(Arrays.toString(populacao[i]));
-        }
+//        for(int i = 0; i <  populacao.length; i++) {
+//            System.out.print("individuo " + i + " - ");
+//            System.out.println(Arrays.toString(populacao[i]));
+//        }
+        ordenarPopulacao(populacao);
     }
    
     
---
+++ src/GA/Individuo.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GA;
+
+/**
+ *
+ * @author WIBSA-PC144
+ */
+public class Individuo {
+   int[] genes = new int[6];
+   int aptidao;
+
+//    public Individuo() {
+//        for(int i = 0; i < genes.length; i++){
+//            genes[i] = 0;
+//        }
+//        aptidao = 0;
+//    }
+
+    public int[] getGenes() {
+        return genes;
+    }
+
+    public void setGenes(int[] genes) {
+        this.genes = genes;
+    }
+
+    public int getAptidao() {
+        return aptidao;
+    }
+
+    public void setAptidao(int aptidao) {
+        this.aptidao = aptidao;
+    }
+}
